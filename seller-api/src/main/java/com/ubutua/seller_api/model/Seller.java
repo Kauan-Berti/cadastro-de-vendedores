ad6886c6fbbb8e1d3ecdb4ad9fe461c3f607b061
Kauan Berti <kauan.g.berti@gmail.com>
2026-05-19 20:02:52 -0300

    feat: created seller entity closed #2

---
+++ seller-api/src/main/java/com/ubutua/seller_api/model/Seller.java
@@ -0,0 +1,59 @@
+package com.ubutua.seller_api.model;
+
+public class Seller {
+
+        private Long id;
+        private String name;
+        private Double salary;
+        private Double bonus;
+        private Integer gender;
+
+
+        public Seller(){
+
+        }
+
+    public Seller(Long id, String name, Double salary, Double bonus, Integer gender) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.bonus = bonus;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Double bonus) {
+        this.bonus = bonus;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+}

1d81089a4dfba31ed0d48fdf166f7d2d8fdc7fee
Kauan Berti <kauan.g.berti@gmail.com>
2026-05-19 01:36:36 -0300

    Init repo closed #1

---
+++ seller-api/src/main/java/com/ubutua/seller_api/SellerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ubutua.seller_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerApiApplication.class, args);
+	}
+
+}
---
+++ seller-api/src/test/java/com/ubutua/seller_api/SellerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubutua.seller_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e2a87bcb7ae08e796b96bc43e89876672ad24cb0
Kauan Berti <kauan.g.berti@gmail.com>
2026-05-25 18:52:52 -0300

    feat: Created Seller component and implemented the form  Closed #10

---
+++ seller-frontend-angular/src/app/components/seller/seller.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Seller } from '../../interfaces/Seller';
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-seller',
+  imports: [BrowserModule, FormsModule],
+  templateUrl: './seller.component.html',
+  styleUrl: './seller.component.css',
+})
+export class SellerComponent {
+
+
+   seller : Seller = {} as Seller;
+
+   genderSelected: string = 'M';
+
+   convert(genderSelected: string): number {
+      return genderSelected === 'M' ? 0 : 1;
+   }
+
+
+  constructor() { };
+
+  save(){
+    this.seller.gender = this.convert(this.genderSelected);
+    console.log(this.seller);
+  }
+
+}

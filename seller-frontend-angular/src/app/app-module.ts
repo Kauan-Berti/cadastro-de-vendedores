f18efd372f1642f98bf7542f18f6d64af1817a63
Kauan Berti <kauan.g.berti@gmail.com>
2026-05-25 01:25:44 -0300

    feat: Created Header and Footer components Closed #9

--- seller-frontend-angular/src/app/app-module.ts
+++ seller-frontend-angular/src/app/app-module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
   declarations: [App],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FooterComponent, HeaderComponent],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
